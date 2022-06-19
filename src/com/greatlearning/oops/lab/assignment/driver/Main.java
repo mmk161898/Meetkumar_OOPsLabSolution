6eb7eee5b2b8f3a9727898a77206ac311558f479
meet <meetmevada1998@gmail.com>
2022-06-19 16:29:20 +0530

    Updated Main class
    I have updated main class with do while loop to get the input if user provide wrong input.

--- src/com/greatlearning/oops/lab/assignment/driver/Main.java
+++ src/com/greatlearning/oops/lab/assignment/driver/Main.java
@@ -9,55 +9,59 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Employee employee = new Employee("Meet", "Mevada");
+		Employee employee = new Employee("Honey", "Mevada");
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please select the department from the following : \n" + "1.Technical \n" + "2.Admin \n"
-				+ "3.Human Resource \n" + "4.Legal \n");
+		int input = 0;
+		do {
 
-		int input = sc.nextInt();
-		String department;
-		String companyName;
-		String generatedPassword;
-		String generatedEmail;
-		CredentialService credentialService = new CredentialService();
+			System.out.println("Please select the department from the following : \n" + "1.Technical \n" + "2.Admin \n"
+					+ "3.Human Resource \n" + "4.Legal \n");
 
-		switch (input) {
-		case 1:
-			department = "tech";
-			System.out.println("Please enter your company name : ");
-			companyName = sc.next();
-			generatedPassword = credentialService.generatePassword();
-			generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
-			credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
-			break;
-		case 2:
-			department = "adm";
-			System.out.println("Please enter your company name : ");
-			companyName = sc.next();
-			generatedPassword = credentialService.generatePassword();
-			generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
-			credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
-			break;
-		case 3:
-			department = "hr";
-			System.out.println("Please enter your company name : ");
-			companyName = sc.next();
-			generatedPassword = credentialService.generatePassword();
-			generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
-			credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
-			break;
-		case 4:
-			department = "legal";
-			System.out.println("Please enter your company name : ");
-			companyName = sc.next();
-			generatedPassword = credentialService.generatePassword();
-			generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
-			credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
-			break;
-		default:
-			System.out.println("Please enter valid department.");
-		}
+			input = sc.nextInt();
+			String department;
+			String companyName;
+			String generatedPassword;
+			String generatedEmail;
+			CredentialService credentialService = new CredentialService();
+
+			switch (input) {
+			case 1:
+				department = "tech";
+				System.out.println("Please enter your company name : ");
+				companyName = sc.next();
+				generatedPassword = credentialService.generatePassword();
+				generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
+				credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
+				break;
+			case 2:
+				department = "adm";
+				System.out.println("Please enter your company name : ");
+				companyName = sc.next();
+				generatedPassword = credentialService.generatePassword();
+				generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
+				credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
+				break;
+			case 3:
+				department = "hr";
+				System.out.println("Please enter your company name : ");
+				companyName = sc.next();
+				generatedPassword = credentialService.generatePassword();
+				generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
+				credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
+				break;
+			case 4:
+				department = "legal";
+				System.out.println("Please enter your company name : ");
+				companyName = sc.next();
+				generatedPassword = credentialService.generatePassword();
+				generatedEmail = credentialService.generateEmailAddress(employee, department, companyName);
+				credentialService.ShowCredentials(employee.getFirstName(), generatedEmail, generatedPassword);
+				break;
+			default:
+				System.out.println("Please enter valid department.");
+			}
+		} while (input != 1 && input != 2 && input != 3 && input != 4);
 		sc.close();
 	}
 
